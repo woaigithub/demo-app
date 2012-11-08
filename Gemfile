4a08ca95e1a0342d677990965a54326be8ad811d
woaigithub <jorden008@gmail.com>
2012-11-08 11:57:42 +0800

    make a basic user model (include secure password)

--- Gemfile
+++ Gemfile
@@ -1,8 +1,8 @@
-#source 'https://rubygems.org'
+source 'https://rubygems.org'
 
-#source 'https://rubygems.org'
+#source 'http://mirrors.tuna.tsinghua.edu.cn/rubygems/'
 
-source 'http://ruby.taobao.org'
+#source 'http://ruby.taobao.org'
 
 gem 'rails', '3.2.8'
 
@@ -10,15 +10,22 @@ gem 'rails', '3.2.8'
 # gem 'rails', :git => 'git://github.com/rails/rails.git'
 
 gem 'bootstrap-sass', '2.0.4'
+gem 'bcrypt-ruby', '3.0.1'
 
 gem 'sqlite3'
 
 gem 'nokogiri', '1.5.5'
-gem 'rspec-rails', '2.11.0'
-gem 'guard-rspec', '1.2.1'
+gem 'rspec-rails', '2.11.0'  # unit-test
+gem 'guard-rspec', '1.2.1'   # auto unit test
 gem 'guard-spork', '1.2.0'
-gem 'spork', '0.9.2'
+gem 'spork', '0.9.2'         # speed unit test
+
+group :development do
+  gem 'annotate', '2.5.0'  # annotate for model
+end
+
 # Gems used only for assets and not required
+
 # in production environments by default.
 group :assets do
   gem 'sass-rails',   '~> 3.2.3'
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,87 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :integer          not null, primary key
+#  name            :string(255)
+#  email           :string(255)
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#  password_digest :string(255)
+#
+
+
+require 'spec_helper'
+
+describe User do
+  before { @user=User.new(:name=>"haha",:email=>"123@asdf.com",:password=>"123",
+                          :password_confirmation=>"123") }
+
+  subject { @user }
+
+  let(:found_user) { User.find_by_email(@user.email) }
+
+  it { should respond_to(:name) }
+  it { should respond_to(:email) }
+  it { should respond_to(:password_digest) }
+  it { should respond_to(:password) }
+  it { should respond_to(:password_confirmation) }
+  it { should respond_to(:authenticate) }
+  it { should be_valid }
+
+  describe "when name is not present" do
+    before { @user.name=" " }
+
+    it { should_not be_valid }
+  end
+
+  describe "when email is not present" do
+    before { @user.email=nil }
+    it { should_not be_valid }
+  end
+
+  describe "when name is too long" do 
+    before { @user.name="a"*50 }
+    it { should_not be_valid }
+  end
+
+  describe "when email format is invalid" do
+    it "should be invalid" do
+      addresses=%w[user@foo,com user_at.org eadf.df@foo.foo@bar.com foo@ba+sdf.com]
+      addresses.each do |invalid_address|
+        @user.email=invalid_address
+        @user.should_not be_valid
+      end
+    end
+  end 
+
+  describe "when email format is valid" do
+    it "should be valid" do
+      addresses=%w[user@foo.com user_@.org eadf.dffoo.foo@bar.com foo@basdf.com]
+      addresses.each do |valid_address|
+        @user.email=valid_address
+        @user.should be_valid
+      end
+    end
+  end
+
+
+  describe "when email address is already taken" do
+    before do
+      user_with_same_email=@user.dup
+      user_with_same_email.email=@user.email.upcase
+      user_with_same_email.save
+    end
+    it { shoud_not be_valid }
+  end
+
+  describe "with invalid password" do
+    let(:user_for_invalid_password) { found_user.authenticate("invalid") }
+    it { should_not ==user_for_invalid_password }
+    specify { user_for_invalid_password.should be_false }
+  end
+ 
+#  pending "add some examples to (or delete) #{__FILE__}"
+end
+
+
